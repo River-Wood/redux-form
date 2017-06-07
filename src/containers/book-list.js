179c720bb7cbd0786c9aec5131aaec78c4ea2516
River-Wood <kinnon.wu@gmail.com>
2017-06-07 17:30:49 +0800

    code

---
+++ src/containers/book-list.js
@@ -0,0 +1,38 @@
+import React, {Component} from 'react';
+import { connect } from 'react-redux';
+import {selectBook } from '../actions/index';
+import { bindActionCreators } from 'redux';
+
+class BookList extends Component {
+    renderList() {
+        return this.props.books.map((book) => {
+            return (
+                <li onClick={() => {this.props.selectBook(book)}} key={book.title} className="list-group-item">{book.title}</li>
+            )
+        })
+    }
+
+    render (){
+        return (
+            <ul className="list-group col-sm-4">
+                {this.renderList()}
+            </ul>
+        )
+    }
+ }
+
+ function mapStateToProps(state){
+     //whatever is returned will show up as props inside of BookList
+     return {
+         books : state.books
+     }
+ }
+
+// anything returned from this function will end up as porps on the booklist container.
+ function mapDispatchToProps(dispatch){
+     // whenever selectbook is called, the result should be passed to all of our reducers.
+     return bindActionCreators( {selectBook : selectBook}, dispatch );
+ }
+// convert booklist from component to container. it needs to know about this new dispatch method. selected
+// book Make it availabel as a prop.
+ export default connect(mapStateToProps, mapDispatchToProps)(BookList);
---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+export default function() {
+    return [
+        { title : 'Javascript', pages : 111},
+        { title : 'Harry', pages : 121},
+        { title : 'Dark Tower', pages : 131},
+        { title : 'Ruby', pages : 11}
+    ]
+}
